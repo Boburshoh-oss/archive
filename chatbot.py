620d758bef8a8c82c00fa48a1774835d371bbe89
Boburshoh-oss <Boburbekbotirov868@gmail.com>
2023-10-11 13:57:29 +0500

    mychatbot

---
+++ chatbot.py
@@ -0,0 +1,42 @@
+import pickle
+import numpy as np
+import json
+from tensorflow import keras
+
+with open("intents_converted_original.json") as file:
+    data = json.load(file)
+
+
+def chat():
+    # load trained model
+    model = keras.models.load_model('chat_model')
+
+    # load tokenizer object
+    with open('tokenizer.pickle', 'rb') as handle:
+        tokenizer = pickle.load(handle)
+
+    # load label encoder object
+    with open('label_encoder.pickle', 'rb') as enc:
+        lbl_encoder = pickle.load(enc)
+
+    # parameters
+    max_len = 20
+
+    while True:
+        print("User: " ,end="")
+        inp = input()
+        if inp.lower() == "quit":
+            break
+
+        result = model.predict(keras.preprocessing.sequence.pad_sequences(tokenizer.texts_to_sequences([inp]),
+                                             truncating='post', maxlen=max_len))
+        tag = lbl_encoder.inverse_transform([np.argmax(result)])
+
+        for i in data['intents']:
+            if i['tag'] == tag:
+                print("ChatBot:" , np.random.choice(i['responses']))
+
+print("Start messaging with the Chatbot (type quit to stop)!")
+
+# call the function to start chat
+chat()
