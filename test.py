9c84b7b08e2bae5aceb3e2d3684d0e913c489abb
rustamovilyos <rustamov7871@gmail.com>
2023-10-16 19:51:33 +0500

    created test.py for testing

---
+++ test.py
@@ -0,0 +1,14 @@
+# text = ("Men, o'zbek tilidagi nutqni va matnlarni sun’iy intellekt yordamida qayta ishlash va o'zbek tilida muloqot"
+#         " qiladigan aqlli yordamchi")
+#
+#
+# def replace_apostrophes(input_text):
+#     input_text = input_text.split()
+#     text_rep = []
+#     for i in input_text:
+#         if i == "'":
+#             text_rep.extend(i.replace("'", "‘"))
+#         else:
+#             text_rep.extend(i)
+#     text_rep = "".join(text_rep)
+#     return text_rep
